8bfa881d7ae012bd750a74223b995b33bd72381b
Durgeshhirani <101794737+Durgeshhirani@users.noreply.github.com>
2025-02-08 14:39:03 +0530

    done.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,8 +10,7 @@ class HomeController extends Controller
     public function users()
     {
         $usersCount = User::count();
-
-        return view('users');
+        return view('users', ['usersCount' => $usersCount]);
     }
 
     // Task 2. Change the View code so alert would not show on the screen
@@ -26,6 +25,7 @@ class HomeController extends Controller
     public function table()
     {
         $users = User::all();
+
 
         return view('table', compact('users'));
     }
